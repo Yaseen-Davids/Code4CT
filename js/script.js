691bb4a68e18936b722a530499e067a9d327401d
Yaseen Davids <yaseendavids477@gmail.com>
2018-06-21 15:15:31 +0200

    Contact page complete (not responsive)

--- js/script.js
+++ js/script.js
@@ -1,82 +1,101 @@
 // This is the slide counter
 let slideNow = 0;
 
+function slide_1() {
+    slideNow = 1;
+    $("#img_1").fadeIn("slow");
+    $("#img_2").fadeOut("slow");
+    $("#img_3").fadeOut("slow");
+}
+function slide_2() {
+    slideNow = 2;
+    $("#img_2").fadeIn("slow");
+    $("#img_1").fadeOut("slow");
+    $("#img_3").fadeOut("slow");
+}
+function slide_3() {
+    slideNow = 3;
+    $("#img_3").fadeIn("slow");
+    $("#img_1").fadeOut("slow");
+    $("#img_2").fadeOut("slow");
+}
+
 // This is the onclick function for the next slide
-function nextSlide(){
+// function nextSlide(){
 
-    // Go to next slide
-    if (slideNow == 0 || slideNow == "0"){
-        $("#img_1").animate({marginLeft: "-100vw"},700);
-        $("#img_2").animate({marginLeft: "100vw"},700);        
-        setTimeout(function(){
-            $("#img_1").hide();
-            $("#img_2").show();
-            $("#img_2").animate({marginLeft: "0vw"},700);
-            slideNow = 1;
-        },700)
-    }
+//     // Go to next slide
+//     if (slideNow == 0 || slideNow == "0"){
+//         $("#img_1").animate({marginLeft: "-100vw"},700);
+//         $("#img_2").animate({marginLeft: "100vw"},700);        
+//         setTimeout(function(){
+//             $("#img_1").hide();
+//             $("#img_2").show();
+//             $("#img_2").animate({marginLeft: "0vw"},700);
+//             slideNow = 1;
+//         },700)
+//     }
 
-    // Go to next slide
-    if (slideNow == 1){
-        $("#img_2").animate({marginLeft: "-100vw"},700);
-        $("#img_3").animate({marginLeft: "100vw"},700);        
-        setTimeout(function(){
-            slideNow = 2;
-            $("#img_2").hide();
-            $("#img_3").show();
-            $("#img_3").animate({marginLeft: "0vw"},700);
-        },700)
-    }
+//     // Go to next slide
+//     if (slideNow == 1){
+//         $("#img_2").animate({marginLeft: "-100vw"},700);
+//         $("#img_3").animate({marginLeft: "100vw"},700);        
+//         setTimeout(function(){
+//             slideNow = 2;
+//             $("#img_2").hide();
+//             $("#img_3").show();
+//             $("#img_3").animate({marginLeft: "0vw"},700);
+//         },700)
+//     }
 
-    // Go to next slide
-    if (slideNow == 2){
-        $("#img_3").animate({marginLeft: "-100vw"},700);
-        $("#img_4").animate({marginLeft: "100vw"},700);        
-        setTimeout(function(){
-            slideNow = 3;
-            $("#img_3").hide();
-            $("#img_4").show();
-            $("#img_4").animate({marginLeft: "0vw"},700);
-        },700)
-    }
-}
+//     // Go to next slide
+//     if (slideNow == 2){
+//         $("#img_3").animate({marginLeft: "-100vw"},700);
+//         $("#img_4").animate({marginLeft: "100vw"},700);        
+//         setTimeout(function(){
+//             slideNow = 3;
+//             $("#img_3").hide();
+//             $("#img_4").show();
+//             $("#img_4").animate({marginLeft: "0vw"},700);
+//         },700)
+//     }
+// }
 
-// This is the onclick function to go to the previous slide
-function backSlide(){
+// // This is the onclick function to go to the previous slide
+// function backSlide(){
 
-    // Go to previous slide
-    if (slideNow == 1){
-        $("#img_2").animate({marginLeft: "100vw"},700);
-        setTimeout(function(){
-            slideNow = 0;
-            $("#img_2").hide();
-            $("#img_1").show();
-            $("#img_1").animate({marginLeft: "0vw"},700);
-        },700)
-    }
+//     // Go to previous slide
+//     if (slideNow == 1){
+//         $("#img_2").animate({marginLeft: "100vw"},700);
+//         setTimeout(function(){
+//             slideNow = 0;
+//             $("#img_2").hide();
+//             $("#img_1").show();
+//             $("#img_1").animate({marginLeft: "0vw"},700);
+//         },700)
+//     }
 
-    // Go to previous slide
-    if (slideNow == 2){
-        $("#img_3").animate({marginLeft: "100vw"},700);
-        setTimeout(function(){
-            slideNow = 1;
-            $("#img_3").hide();
-            $("#img_2").show();
-            $("#img_2").animate({marginLeft: "0vw"},700);
-        },700)
-    }
+//     // Go to previous slide
+//     if (slideNow == 2){
+//         $("#img_3").animate({marginLeft: "100vw"},700);
+//         setTimeout(function(){
+//             slideNow = 1;
+//             $("#img_3").hide();
+//             $("#img_2").show();
+//             $("#img_2").animate({marginLeft: "0vw"},700);
+//         },700)
+//     }
 
-    // Go to previous slide
-    if (slideNow == 3){
-        $("#img_4").animate({marginLeft: "100vw"},700);
-        setTimeout(function(){
-            slideNow = 2;
-            $("#img_4").hide();
-            $("#img_3").show();
-            $("#img_3").animate({marginLeft: "0vw"},700);
-        },700)
-    }
-}
+//     // Go to previous slide
+//     if (slideNow == 3){
+//         $("#img_4").animate({marginLeft: "100vw"},700);
+//         setTimeout(function(){
+//             slideNow = 2;
+//             $("#img_4").hide();
+//             $("#img_3").show();
+//             $("#img_3").animate({marginLeft: "0vw"},700);
+//         },700)
+//     }
+// }
 
 // This is my simple Smooth scroller, this takes to you to the top of the page when clicked
 function backToTop() {
@@ -249,3 +268,15 @@ onload = function countUpStatistics() {
     
 }
 
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -33.924692, lng: 18.448723};
+    // -33.924692, 18.448723
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('contact_map_box'), {zoom: 18, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
+  initMap();
